9414489b69a995470b0685e27b6ea608300b0207
Suryansh <suryanshgrover1999@gmail.com>
2022-11-03 14:15:33 +0530

    upgraded a bit

--- app.py
+++ app.py
@@ -1,28 +1,33 @@
-# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
-# .ml/scripts/activate
-
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS, cross_origin
 from regress import LModel
 import logging as lg
 from logger import Log
 
-app = Flask(__name__)  # intializing our Flask app
+app = Flask(__name__)  # intializing the Flask app
 
-# executing the logger class
+# Executing the logger class
 Log()
 
 @app.route('/', methods=['GET'])
 @cross_origin()
-def homepage(): 
-    return render_template("index.html")
+def homepage():
+    try:
+        return render_template("index.html")
+    except Exception as e:
+        lg.exception(e)
+        error_m1 = "There's some error loading the Analytics Report."
+        error_m2 = "Kindly try again after some time!"
+        return render_template('errors.html', error_m1=error_m1, error_m2=error_m2)
 
-
-@app.route('/dataset+report', methods=['Get'])
+@app.route('/dataset+analytics', methods=['Get'])
 @cross_origin()
-def dataset_report():
-    return render_template("dataset_report.html")
-
+def dataset_analytics():
+    try:
+        lg.info("rendering dataset analytics..")
+        return render_template("dataset_report.html")
+    except Exception as e:
+        lg.exception(e)
 
 @app.route('/prediction+results', methods=['POST', 'GET'])
 @cross_origin()
@@ -46,13 +51,16 @@ def main():
             # loading our Regression model
             lm = LModel()
             lm.build()
-            print("done !")
             prediction = lm.predict(process_temp, rot_speed, torque, tool_wear, machine_f, twf, hdf, pwf, osf, rnf)
 
         except Exception as e:
-            return render_template('errors.html', errors=e)
+            lg.exception(e)
+            error_m1 = "It's either that you have rendered some inputs empty or its values are out of the range."
+            error_m2 = "Please try again with apt values!"
+            return render_template('errors.html', error_m1=error_m1, error_m2=error_m2)
 
         else:
+            lg.info("rendering results..")
             return render_template('results.html', prediction=round(prediction, 3), accuracy=lm.accuracy())
 
 
